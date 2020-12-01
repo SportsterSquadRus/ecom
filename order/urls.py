e73c4d9016bcf64a2392c968b3eea37770452c68
suren <a3a3a@list.ru>
2020-12-01 14:52:08 +0300

    order update url

--- order/urls.py
+++ order/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('list/', views.OrdersListView.as_view(), name='order_list_url'),
     path('create/', views.OrderCreateView.as_view(), name='order_create_url'),
-    path('detail/<int:pk>/', views.OrderDetailView.as_view(),name='order_detail_url')
+    path('detail/<int:pk>/', views.OrderDetailView.as_view(),name='order_detail_url'),
+    path('update/<int:pk>/', views.OrderUpdateView.as_view(),name='order_update_url')
 ]
--- order/views.py
+++ order/views.py
@@ -1,8 +1,8 @@
 """Модуль обработки запросов"""
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from .models import Order
 from .forms import OrderForm
-# from django.shortcuts import redirect, reverse
+
 
 class OrdersListView(ListView):
     """Вывод всех заказов"""
@@ -12,15 +12,23 @@ class OrdersListView(ListView):
     context_object_name = 'orders'
     paginate_by = 4
 
+
 class OrderCreateView(CreateView):
     """Создание заказа"""
     model = Order
     form_class = OrderForm
-    # success_url = '/'
     template_name = 'order/order_create.html'
+
 
 class OrderDetailView(DetailView):
     """Детальная информация о заказе"""
     model = Order
     template_name = 'order/order_detail.html'
     context_object_name = 'order'
+
+
+class OrderUpdateView(UpdateView):
+    """Изменение заказа"""
+    model = Order
+    template_name = 'order/order_create.html'
+    form_class = OrderForm
