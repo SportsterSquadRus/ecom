a97c1484fa1c18f279c802215d05a9762b4ce734
suren <a3a3a@list.ru>
2020-12-01 18:58:08 +0300

    code cleaning

--- order/models.py
+++ order/models.py
@@ -8,7 +8,8 @@ class Order(models.Model):
     date = models.DateField(verbose_name='Дата заказа')
     agent = models.CharField(max_length=100, verbose_name='Контрагент')
     comment = models.CharField(max_length=150, verbose_name='Текст заказа')
-    amount = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Сумма заказа')
+    amount = models.DecimalField(
+        max_digits=9, decimal_places=2, verbose_name='Сумма заказа')
 
     class Meta:
         ordering = ['-date']
